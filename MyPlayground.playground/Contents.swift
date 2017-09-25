481e6e102ea31a024cf5d4412a82b8daae1ab15e
greenRose2018 <brifun13@gmail.com>
2017-09-25 08:32:58 -0600

    Learning to use the while statement for fun

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,4 +3,13 @@
 import UIKit
 
 var str = "Hello, playground"
-int me = 
+str = "bonjour"
+
+var spanish : String = " Hola "
+
+print(str + spanish)
+
+var num : Int = 0
+
+while ( num <= 8)
+{ num += 2; print(num)}
