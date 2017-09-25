53f0ce56f58f6649835d7cc9771563565250633d
greenRose2018 <brifun13@gmail.com>
2017-09-25 08:32:23 -0600

    learning to Declare varibles and practing using swift

---
+++ IntroSwift.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Explicit Type ddeclaration with no value
+var ramen :String
+//Implicit Type deration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge:Int
+myAge = -87
+print(myAge)
+myAge = myAge * 3
+print(myAge)
+
+var someValue= 3.1415*16
+someValue =someValue * 231.1
+
+var is Real :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Brit"
+
+print
