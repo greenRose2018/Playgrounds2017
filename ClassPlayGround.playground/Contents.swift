e83e36f16ddc1a54f20bdcafb19ba697e7563db5
greenRose2018 <brifun13@gmail.com>
2017-10-02 09:08:25 -0600

    Practicing using Int and String with init and func

--- ClassPlayGround.playground/Contents.swift
+++ ClassPlayGround.playground/Contents.swift
@@ -30,4 +30,28 @@ print(mySample.getMyName())
 mySample.setMyName(name: "hello my friends")
 print(mySample.getMyName())
 
-
+public class OtherClass
+{
+    private var favoriteNumber :Int
+    private var favoriteWord :String
+    
+    public init()
+    {
+        favoriteNumber = Int()
+        favoriteWord = String()
+    }
+    public init (favoriteNumber :Int, favoriteWord :String)
+    {
+        self.favoriteNumber = favoriteNumber
+        self.favoriteWord = favoriteWord
+    }
+    
+    public func changeValues() -> Void
+    {
+        self.favoriteNumber += 76
+        self.favoriteWord += " more and more"
+    }
+}
+var firstSample = OtherClass()
+var secondSample = OtherClass(favoriteNumber:13, favoriteWord: "bork")
+secondSample.changeValues()
