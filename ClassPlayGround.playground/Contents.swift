ab75aee4f3019d87e34b1a84ada08a9ddc096200
greenRose2018 <brifun13@gmail.com>
2017-10-02 08:39:52 -0600

    How to Set and Get and call classes and init

--- ClassPlayGround.playground/Contents.swift
+++ ClassPlayGround.playground/Contents.swift
@@ -11,7 +11,23 @@ public class SimpleClass
     public init()
     {
         myName = "words in here"
-        print(myName)
+        
+    }
+    
+    public func getMyName() -> String       //a getter, a mple and easy way
+    {
+        return myName
+    }
+    
+    public func setMyName(name :String) -> Void
+    {
+        myName = name
     }
 }
 var mySample :SimpleClass = SimpleClass() //this calls the init
+print(mySample.getMyName())
+
+mySample.setMyName(name: "hello my friends")
+print(mySample.getMyName())
+
+
