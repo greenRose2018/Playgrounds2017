31f131572254bdfe0542115653f75cf0d1f60ccc
greenRose2018 <brifun13@gmail.com>
2017-10-10 08:17:28 -0600

    added a label

--- GUIPlayground.playground/Contents.swift
+++ GUIPlayground.playground/Contents.swift
@@ -15,10 +15,13 @@ secondButton.backgroundColor = .yellow
 secondButton.setTitle("yay", for: .normal)
 secondButton.setTitleColor(.red, for: .normal)
 
-var firstLabel: UILabel = UILabel()
-firstLabel.frame = CGRect(x:50, y:150, width:200, height:21)
+var firstLabel : UILabel = UILabel(frame: CGRect(x:50, y:150, width:200, height:21))
 firstLabel.backgroundColor = .purple
-firstLabel.textColor = .white
-firstLabel.textAlignment = .center
+firstLabel.textColor = UIColor.white
+firstLabel.textAlignment = NSTextAlignment.center
 firstLabel.text = "hello there"
+var secondLabel: UILabel =  UILabel(frame: CGRect(x:100, y:100, width:200, height:50))
+secondLabel.backgroundColor = .orange
 
+//var frame = CGRect(x:45,y:45, width:20, height:20)
+//var backgroundColor = .black
